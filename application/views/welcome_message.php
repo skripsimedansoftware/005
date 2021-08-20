9e6c0f0f276a29bd7d00c2ce5901fbdca17cf779
Agung Dirgantara <agungmasda29@gmail.com>
2021-08-21 04:29:42 +0700

    change default controller display

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to Sistem Administrasi Tugas Akhir & Kerja Praktek</title>
 
 	<style type="text/css">
 
@@ -68,18 +68,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to Sistem Administrasi Tugas Akhir & Kerja Praktek!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>Login <a href="<?php echo base_url('admin') ?>">admin</a></p>
+		<p>Login <a href="<?php echo base_url('dosen') ?>">dosen</a></p>
+		<p>Login <a href="<?php echo base_url('mahasiswa') ?>">mahasiswa</a></p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
