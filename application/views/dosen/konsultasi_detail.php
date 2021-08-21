fb9ce80212266488c31dc7adbdae15027fe21b1c
Agung <agungmasda29@gmail.com>
2021-08-21 11:31:20 +0700

    update title

--- application/views/dosen/konsultasi_detail.php
+++ application/views/dosen/konsultasi_detail.php
@@ -1,6 +1,6 @@
 <!-- Content Header (Page header) -->
 <section class="content-header">
-	<h1>Halaman<small>Dosen</small></h1>
+	<h1>Dosen<small>Konsultasi Mahasiswa</small></h1>
 </section>
 
 <!-- Main content -->
